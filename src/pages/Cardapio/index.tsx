8761becb7735a6851fb8f135325b45fd17a9ce96
Vinícius Yoda <vyoda4604@gmail.com>
2023-08-05 11:39:33 -0300

    performance

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,11 +1,24 @@
-import styles from "./Cardapio.module.scss";
+import styles from './Cardapio.module.scss';
+import Buscador from './Buscador';
+import { useState } from 'react';
+import Filtros from './Filtros';
+import Ordenador from './Ordenador';
+import Itens from './Itens';
+import stylesTema from 'styles/Tema.module.scss';
 
 export default function Cardapio() {
+  const [busca, setBusca] = useState('');
+  const [filtro, setFiltro] = useState<number | null>(null);
+  const [ordenador, setOrdenador] = useState('');
   return (
-    <main>
-      <nav className="menu">
-
-      </nav>
-    </main>
-  )
+    <section className={styles.cardapio}>
+      <h3 className={stylesTema.titulo}>Cardápio</h3>
+      <Buscador busca={busca} setBusca={setBusca} />
+      <div className={styles.cardapio__filtros}>
+        <Filtros filtro={filtro} setFiltro={setFiltro} />
+        <Ordenador ordenador={ordenador} setOrdenador={setOrdenador} />
+      </div>
+      <Itens busca={busca} filtro={filtro} ordenador={ordenador} />
+    </section>
+  );
 }
